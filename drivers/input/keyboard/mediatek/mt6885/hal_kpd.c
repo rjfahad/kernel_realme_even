bf1ab8e95339c678a5acc54cc5941210031eae34
Sarthak Roy <sarthakroy2002@gmail.com>
2026-02-14 13:18:39 +0000

    treewide: Drop OPLUS theia
    * unused
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I2ccec89f258f8afe3c3a80dc40c3d59e49ef57c7

--- drivers/input/keyboard/mediatek/mt6885/hal_kpd.c
+++ drivers/input/keyboard/mediatek/mt6885/hal_kpd.c
@@ -20,11 +20,6 @@
 #include <kpd.h>
 #include <hal_kpd.h>
 #include <mt-plat/mtk_boot_common.h>
-#if IS_ENABLED(CONFIG_OPLUS_FEATURE_THEIA)
-//Yang.Gang@BSP.Kernel.Stability,add 2020/06/08 for BScheck
-#include <soc/oplus/system/oplus_bscheck.h>
-#include <soc/oplus/system/oplus_brightscreen_check.h>
-#endif
 
 #ifdef CONFIG_MTK_PMIC_NEW_ARCH /*for pmic not ready*/
 static int kpd_enable_lprst = 1;
@@ -200,16 +195,7 @@ void kpd_pmic_pwrkey_hal(unsigned long pressed)
 	input_report_key(kpd_input_dev, kpd_dts_data.kpd_sw_pwrkey, pressed);
 	input_sync(kpd_input_dev);
 	kpd_print(KPD_SAY "(%s) HW keycode =%d using PMIC\n",
-	       pressed ? "pressed" : "released", kpd_dts_data.kpd_sw_pwrkey);
-
-    #if IS_ENABLED(CONFIG_OPLUS_FEATURE_THEIA)
-    //Yang.Gang@BSP.Kernel.Stability,add 2020/06/08 for BScheck
-    if(pressed){
-        //we should canel per work
-        black_screen_timer_restart();
-        bright_screen_timer_restart();
-    }
-    #endif		   
+	       pressed ? "pressed" : "released", kpd_dts_data.kpd_sw_pwrkey);	   
 }
 
 static int mrdump_eint_state;
